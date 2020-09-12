3956fd2718fe065295f7c6eb80a4aa1d6e69c3cd
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2020-09-12 10:40:14 +0300

    Add files via upload

---
+++ Source/Packs/PackMapComponents/PackDriver/ThingComps/PackProps.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+namespace AvaliMod
+{
+    public class PackProps : CompProperties
+    {
+        public List<ThingDef> racesInPacks;
+        public bool canHaveAloneThought;
+        public ThoughtDef aloneThought;
+        public PawnRelationDef relation;
+        public SimpleCurve packGenChanceOverAge;
+        public PackProps()
+        {
+            this.compClass = typeof(PackComp);
+        }
+    }
+}
