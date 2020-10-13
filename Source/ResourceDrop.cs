a1aed480f982c17b27073da3e24c11173a0fdd37
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2020-10-13 15:28:44 -0400

    Update ResourceDrop.cs

--- Source/ResourceDrop.cs
+++ Source/ResourceDrop.cs
@@ -6,14 +6,21 @@ namespace AvaliMod
 {
     public class IlluminateAirdrop : IncidentWorker
     {
+        public book hasDropped;
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref hasDropped, “hasDropped", false);
+            base.ExposeData();
+        }
         protected override bool TryExecuteWorker(IncidentParms parms)
         {
             List<Thing> thingList = new List<Thing>();
             thingList.Add(ThingMaker.MakeThing(AvaliDefs.AvaliNanoForge));
             Map target = (Map)parms.target;
             IntVec3 intVec3 = DropCellFinder.TradeDropSpot(target);
-            if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5)
+            if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5 && !hasDropped)
             {
+                hasDropped = true;
                 DropPodUtility.DropThingsNear(intVec3, target, (IEnumerable<Thing>)thingList);
                 this.SendStandardLetter("Illuminate Airdrop", "Illuminate Airdrop", AvaliDefs.IlluminateAirdrop,parms, (LookTargets)new TargetInfo(intVec3, target, false), (NamedArgument[])Array.Empty<NamedArgument>());
             }
