33047f184dfd348d11fde6bb4af249bf8913e91a
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2020-11-03 23:26:18 -0500

    Add files via upload

---
+++ Source/Airdrop.cs
@@ -0,0 +1,47 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+namespace AvaliMod
+{
+    public class AirDropHandler : MapComponent
+    {
+        private int ticks = 0;
+        public AirDropHandler(Map map)
+            : base(map)
+        {
+
+        }
+
+        public bool hasDropped;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref hasDropped, "hasDropped", false);
+            base.ExposeData();
+        }
+        private void SendDrop()
+        {
+            List<Thing> thingList = new List<Thing>();
+            thingList.Add(ThingMaker.MakeThing(AvaliDefs.AvaliNanoForge));
+            Scribe_Values.Look(ref hasDropped, "hasDropped", false);
+            Map target = map;
+            IntVec3 intVec3 = DropCellFinder.TradeDropSpot(target);
+            if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5 && !hasDropped)
+            {
+                hasDropped = true;
+                DropPodUtility.DropThingsNear(intVec3, target, (IEnumerable<Thing>)thingList);
+       
+            }
+        }
+        public override void MapComponentTick()
+        {
+
+            if (ticks == 120)
+            {
+                SendDrop();
+                ticks = 0;
+            }
+        }
+    }
+}
