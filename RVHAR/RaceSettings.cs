94e1599c782604ec1afb605218718d7ab494a621
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2020-11-03 23:21:03 -0500

    Add files via upload
    Added RV-HAR
    Removed old resourcedrop, replaced by airdrop

---
+++ RVHAR/RaceSettings.cs
@@ -0,0 +1,50 @@
+﻿namespace RVHAR
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using RimWorld;
+    using Verse;
+
+    [StaticConstructorOnStartup]
+    public class RaceSettings : Def
+    {
+        public PawnKindSettings pawnKindSettings;
+        public List<BackstoryTagItem> backstoryTagInsertion;
+
+        static RaceSettings()
+        {
+            foreach (BackstoryTagItem bt in DefDatabase<RaceSettings>.AllDefs.SelectMany(selector: rs => rs.backstoryTagInsertion))
+            {
+                foreach (string backstory in bt.backstories)
+                    if (BackstoryDatabase.TryGetWithIdentifier(backstory, out Backstory bs))
+                        bs.spawnCategories.AddRange(bt.spawnCategories);
+            }
+        }
+    }
+
+    public class BackstoryTagItem
+    {
+        public List<string> backstories = new List<string>();
+        public List<string> spawnCategories = new List<string>();
+    }
+
+    public class PawnKindSettings
+    {
+        public List<PawnKindEntry> alienslavekinds = new List<PawnKindEntry>();
+        public List<PawnKindEntry> alienrefugeekinds = new List<PawnKindEntry>();
+        public List<FactionPawnKindEntry> startingColonists = new List<FactionPawnKindEntry>();
+        public List<FactionPawnKindEntry> alienwandererkinds = new List<FactionPawnKindEntry>();
+    }
+
+    public class PawnKindEntry
+    {
+        public List<string> kindDefs = new List<string>();
+        public float chance;
+    }
+
+    public class FactionPawnKindEntry
+    {
+        public List<PawnKindEntry> pawnKindEntries = new List<PawnKindEntry>();
+        public List<string> factionDefs = new List<string>();
+    }
+}
---
+++ Verbs/ExplodeBombVerb.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+namespace AvaliMod
+{
+    public class Verb_Explode : Verb
+    {
+        protected override bool TryCastShot()
+        {
+            Verb_Explode.Explode(this.ReloadableCompSource);
+            return true;
+        }
+        public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+        {
+            needLOSToCenter = false;
+            return this.EquipmentSource.GetStatValue(AvaliDefs.ExplodeBombRadius, true);
+        }
+
+        public static void Explode(CompReloadable comp)
+        {
+            if (comp == null || !comp.CanBeUsed)
+                return;
+            ThingWithComps parent = comp.parent;
+            Pawn wearer = comp.Wearer;
+            GenExplosion.DoExplosion(wearer.Position, wearer.Map, parent.GetStatValue(AvaliDefs.ExplodeBombRadius, true), DamageDefOf.Bomb, (Thing)null, -1, -1f, (SoundDef)null, (ThingDef)null, (ThingDef)null, (Thing)null, (ThingDef)null, 0.0f, 1, false, (ThingDef)null, 0.0f, 1, 0.0f, false, new float?(), (List<Thing>)null);
+            GenExplosion.DoExplosion(wearer.Position, wearer.Map, 1f, DamageDefOf.Bomb, (Thing)null, -1, -1f, (SoundDef)null, (ThingDef)null, (ThingDef)null, (Thing)null, (ThingDef)null, 0.0f, 1, false, (ThingDef)null, 0.0f, 1, 0.0f, false, new float?(), (List<Thing>)null);
+
+            comp.UsedOnce();
+
+        }
+    }
+}
