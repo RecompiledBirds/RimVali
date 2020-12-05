9d3faf4cdc40d4b4ced229388a46d29398b33395
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2020-12-05 17:20:19 -0500

    Add files via upload

--- Source/Airdrop.cs
+++ Source/Airdrop.cs
@@ -2,15 +2,11 @@
 using System;
 using System.Collections.Generic;
 using Verse;
-using System.Linq;
 namespace AvaliMod
 {
     public class AirDropHandler : MapComponent
     {
-        private readonly bool airdrops = LoadedModManager.GetMod<RimValiMod>().GetSettings<RimValiModSettings>().enableAirdrops;
-        private System.Random random = new System.Random();
         private int ticks = 0;
-        private bool hasStarted = false;
         public AirDropHandler(Map map)
             : base(map)
         {
@@ -18,7 +14,7 @@ namespace AvaliMod
         }
 
         public bool hasDropped;
-        
+
         public override void ExposeData()
         {
             Scribe_Values.Look(ref hasDropped, "hasDropped", false);
@@ -26,39 +22,25 @@ namespace AvaliMod
         }
         private void SendDrop()
         {
-            GenDate.DayOfYear(ticks, Find.WorldGrid.LongLatOf(map.Tile).x);
             List<Thing> thingList = new List<Thing>();
-            thingList.Add(ThingMaker.MakeThing(AvaliDefs.AvaliNexus));
+            thingList.Add(ThingMaker.MakeThing(AvaliDefs.AvaliNanoForge));
             Scribe_Values.Look(ref hasDropped, "hasDropped", false);
             Map target = map;
-            List<Faction> newFactions = new List<Faction>();
             IntVec3 intVec3 = DropCellFinder.TradeDropSpot(target);
-            if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5 && !hasDropped && map.IsPlayerHome)
+            if (RimValiUtility.PawnOfRaceCount(Faction.OfPlayer, AvaliDefs.RimVali) >= 5 && !hasDropped)
             {
                 hasDropped = true;
-                for(int a = 0; a < random.Next(2, 5); a++)
-                {
-                    Faction faction = FactionGenerator.NewGeneratedFaction(AvaliDefs.AvaliFaction);
-                    faction.Name = "IlluminateFactionName".Translate()+": #"+a.ToString();
-                    faction.SetRelationDirect(Faction.OfPlayer, FactionRelationKind.Ally);
-                    newFactions.Add(faction);
-                }
                 DropPodUtility.DropThingsNear(intVec3, target, (IEnumerable<Thing>)thingList);
-                ChoiceLetter choiceLetter = LetterMaker.MakeLetter("IlluminateAirdrop".Translate(), "AirdropEventDesc".Translate(), AvaliMod.AvaliDefs.IlluminateAirdrop,newFactions[random.Next(newFactions.Count)]);
-                Find.LetterStack.ReceiveLetter(choiceLetter, null);
+       
             }
         }
-
         public override void MapComponentTick()
         {
-            ticks++;
+
             if (ticks == 120)
             {
-                if (airdrops)
-                {
-                    SendDrop();
-                    ticks = 0;
-                }
+                SendDrop();
+                ticks = 0;
             }
         }
     }
---
+++ Source/RVHAR/ScenPart_StartingHumanlikes.cs
@@ -0,0 +1,102 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace RVHAR
+{
+    [StaticConstructorOnStartup]
+    public class ScenPart_StartingHumanlikes : ScenPart
+    {
+
+        static ScenPart_StartingHumanlikes()
+        {
+            ScenPartDef scenPart = new ScenPartDef()
+            {
+                defName = "StartingHumanlikes",
+                label = "Start with humanlikes",
+                scenPartClass = typeof(ScenPart_StartingHumanlikes),
+                category = ScenPartCategory.StartingImportant,
+                selectionWeight = 1.0f,
+                summaryPriority = 10
+            };
+            scenPart.ResolveReferences();
+            scenPart.PostLoad();
+            DefDatabase<ScenPartDef>.Add(scenPart);
+        }
+
+        private PawnKindDef kindDef;
+        private int pawnCount;
+        private string buffer;
+
+        public PawnKindDef KindDef
+        {
+            get => this.kindDef = this.kindDef ?? PawnKindDefOf.Villager;
+            set => this.kindDef = value;
+        }
+
+        public override void DoEditInterface(Listing_ScenEdit listing)
+        {
+            Rect scenPartRect = listing.GetScenPartRect(this, RowHeight * 3f);
+            if (Widgets.ButtonText(scenPartRect.TopPart(pct: 0.45f), this.KindDef.label.CapitalizeFirst()))
+            {
+                List<FloatMenuOption> list = new List<FloatMenuOption>();
+                list.AddRange(DefDatabase<RaceSettings>.AllDefsListForReading.Where(predicate: ar => ar.pawnKindSettings.startingColonists != null)
+                   .SelectMany(selector: ar => ar.pawnKindSettings.startingColonists.SelectMany(selector: ste => ste.pawnKindEntries.SelectMany(selector: pke => pke.kindDefs)))
+                   .Where(predicate: s => DefDatabase<PawnKindDef>.GetNamedSilentFail(s) != null).Select(DefDatabase<PawnKindDef>.GetNamedSilentFail)
+                   .Select(selector: pkd => new FloatMenuOption(pkd.label.CapitalizeFirst(), action: () => this.KindDef = pkd)));
+                list.Add(new FloatMenuOption(label: "Villager", action: () => this.KindDef = PawnKindDefOf.Villager));
+                list.Add(new FloatMenuOption(label: "Slave", action: () => this.KindDef = PawnKindDefOf.Slave));
+                Find.WindowStack.Add(new FloatMenu(list));
+            }
+
+            Widgets.TextFieldNumeric(scenPartRect.BottomPart(pct: 0.45f), ref this.pawnCount, ref this.buffer);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.pawnCount, label: "alienRaceScenPawnCount");
+            Scribe_Defs.Look(ref this.kindDef, label: "PawnKindDefAlienRaceScen");
+        }
+
+        public override string Summary(Scenario scen) => ScenSummaryList.SummaryWithList(scen, tag: "PlayerStartsWith", ScenPart_StartingThing_Defined.PlayerStartWithIntro);
+
+        public override IEnumerable<string> GetSummaryListEntries(string tag)
+        {
+            if (tag == "PlayerStartsWith") yield return this.KindDef.LabelCap + " x" + this.pawnCount;
+        }
+
+        public override bool TryMerge(ScenPart other)
+        {
+            if (!(other is ScenPart_StartingHumanlikes others) || others.KindDef != this.KindDef)
+            {
+                return false;
+            }
+
+            this.pawnCount += others.pawnCount;
+            return true;
+        }
+
+        public IEnumerable<Pawn> GetPawns()
+        {
+            bool PawnCheck(Pawn p) => p != null && DefDatabase<WorkTypeDef>.AllDefsListForReading.Where(predicate: wtd => wtd.requireCapableColonist).ToList().TrueForAll(match: w => !p.WorkTypeIsDisabled(w));
+
+
+            for (int i = 0; i < this.pawnCount; i++)
+            {
+                Pawn newPawn = null;
+                for (int x = 0; x < 200; x++)
+                {
+                    newPawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(this.KindDef, Faction.OfPlayer, PawnGenerationContext.PlayerStarter, tile: -1, forceGenerateNewPawn: true, newborn: false, allowDead: false, allowDowned: false, canGeneratePawnRelations: true, mustBeCapableOfViolence: false, colonistRelationChanceFactor: 26f, forceAddFreeWarmLayerIfNeeded: true));
+                    if (PawnCheck(newPawn))
+                    {
+                        x = 200;
+                    }
+                }
+                yield return newPawn;
+            }
+        }
+    }
+}
