47d9b14df2104f4ab3690d39198be5d1f6f88c74
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2021-05-16 18:09:45 -0400

    Add files via upload

--- Source/DeathWorker.cs
+++ Source/DeathWorker.cs
@@ -7,10 +7,7 @@ namespace AvaliMod
     // Token: 0x020000DC RID: 220
     public class DeathActionWorker_Test : DeathActionWorker
     {
-<<<<<<< HEAD
-=======
         AvaliPackDriver AvaliPackDriver = Current.Game.GetComponent<AvaliPackDriver>();
->>>>>>> beta
         private readonly bool enableDebug = LoadedModManager.GetMod<RimValiMod>().GetSettings<RimValiModSettings>().enableDebugMode;
         public override void PawnDied(Corpse corpse)
         {
@@ -18,14 +15,9 @@ namespace AvaliMod
             {
                 Log.Message("Death detected");
             }
-<<<<<<< HEAD
-            if (corpse.InnerPawn.def.race == AvaliDefs.RimVali.race)
-            {
-=======
             if (corpse.InnerPawn.def.HasComp(typeof(PackComp)))
             {
                
->>>>>>> beta
                 if (!(AvaliPackDriver.packs == null) && AvaliPackDriver.packs.Count > 0)
                 {
                     if (enableDebug)
@@ -41,21 +33,14 @@ namespace AvaliMod
                             {
                                 Log.Message("The pawn had a pack");
                             }
-<<<<<<< HEAD
-                            DeathDate deathDate = new DeathDate();
-=======
                             DeathDate deathDate = new DeathDate(pawn);
->>>>>>> beta
                             Log.Message(GenDate.DayOfYear(1, Find.WorldGrid.LongLatOf(corpse.Map.Tile).x).ToString());
                             deathDate.day = GenDate.DayOfYear(1, Find.WorldGrid.LongLatOf(corpse.Map.Tile).x);
                             if(corpse.InnerPawn != null && enableDebug)
                             {
                                 Log.Message("Pawn is not null");
                             }
-<<<<<<< HEAD
-=======
                             
->>>>>>> beta
                             deathDate.deadPawn = pawn;
                             if (enableDebug)
                             {
@@ -63,21 +48,6 @@ namespace AvaliMod
                                 Log.Message(deathDate.day.ToString());
                             }
                             pack.deathDates.Add(deathDate);
-<<<<<<< HEAD
-                            pack.size--;
-                            if (enableDebug)
-                            {
-                                Log.Message(pack.size.ToString());
-                            }
-                            foreach(Pawn packmate in pack.pawns)
-                            {
-                                if(!(pawn == packmate) && !pawn.Dead)
-                                {
-                                    PackComp comp = packmate.TryGetComp<PackComp>();
-                                    if(comp.Props.deathThought != null)
-                                    {
-                                        packmate.needs.mood.thoughts.memories.TryGainMemory(comp.Props.deathThought);
-=======
                             //pack.size--;
                             if (enableDebug)
                             {
@@ -93,7 +63,6 @@ namespace AvaliMod
                                     if(comp.Props.deathThought != null)
                                     {
                                         packmate.needs.mood.thoughts.memories.TryGainMemory(comp.Props.deathThought, pawn);
->>>>>>> beta
                                     }
                                 }
                             }
