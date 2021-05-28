184458e1783e92f386db78c4cc573b73f3558d7b
NesiTheAvali <52045489+NesiTheAvali@users.noreply.github.com>
2021-05-28 09:15:38 -0400

    Add files via upload

---
+++ Source_Folder/DeathWorker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace AvaliMod
+{
+    // Token: 0x020000DC RID: 220
+    public class DeathActionWorker_Test : DeathActionWorker
+    {
+        AvaliPackDriver AvaliPackDriver = Current.Game.GetComponent<AvaliPackDriver>();
+        private readonly bool enableDebug = LoadedModManager.GetMod<RimValiMod>().GetSettings<RimValiModSettings>().enableDebugMode;
+        public override void PawnDied(Corpse corpse)
+        {
+            if (enableDebug)
+            {
+                Log.Message("Death detected");
+            }
+            if (corpse.InnerPawn.def.HasComp(typeof(PackComp)))
+            {
+               
+                if (!(AvaliPackDriver.packs == null) && AvaliPackDriver.packs.Count > 0)
+                {
+                    if (enableDebug)
+                    {
+                        Log.Message("I got here, packs exist.");
+                    }
+                    Pawn pawn = corpse.InnerPawn;
+                    foreach (AvaliPack pack in AvaliPackDriver.packs)
+                    {
+                        if (pack.pawns.Contains(pawn))
+                        {
+                            if (enableDebug)
+                            {
+                                Log.Message("The pawn had a pack");
+                            }
+                            DeathDate deathDate = new DeathDate(pawn);
+                            //Log.Message(GenDate.DayOfYear(1, Find.WorldGrid.LongLatOf(corpse.Map.Tile).x).ToString());
+                            deathDate.day = GenDate.DayOfYear(1, Find.WorldGrid.LongLatOf(corpse.Map.Tile).x);
+                            if(corpse.InnerPawn != null && enableDebug)
+                            {
+                                Log.Message("Pawn is not null");
+                            }
+                            
+                            deathDate.deadPawn = pawn;
+                            if (enableDebug)
+                            {
+                                Log.Message("I got the deathdate");
+                                Log.Message(deathDate.day.ToString());
+                            }
+                            pack.deathDates.Add(deathDate);
+                            pack.pawns.Remove(pawn);
+                            //pack.size--;
+                            if (enableDebug)
+                            {
+                                Log.Message(pack.pawns.Count.ToString());
+                            }
+                            foreach(Pawn packmate in pack.pawns)
+                            {
+                                Log.Message("On: "+ packmate.Name.ToString());
+                                if((pawn != packmate) && !packmate.Dead)
+                                {
+                                    Log.Message(packmate.Name.ToString() + " is not dead, adding thought");
+                                    PackComp comp = packmate.TryGetComp<PackComp>();
+                                    if(comp.Props.deathThought != null)
+                                    {
+                                        packmate.needs.mood.thoughts.memories.TryGainMemory(comp.Props.deathThought, pawn);
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source_Folder/ModScanner.cs
@@ -0,0 +1,81 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+namespace AvaliMod
+{
+    [StaticConstructorOnStartup]
+    public static class Modulefinder
+    {
+        public static IEnumerable<ModContentPack> loadedMods = LoadedModManager.RunningMods.Where(x => x.Name.ToLower().Contains("rimvali"));
+        public static IEnumerable<ModuleDef> moduleDefs = DefDatabase<ModuleDef>.AllDefs;
+        public static void startup()
+        {
+            moduleDefs = DefDatabase<ModuleDef>.AllDefs;
+            foreach (ModuleDef module in moduleDefs)
+            {
+                if (!RimValiUtility.modulesFound.ToLower().Contains(module.name.ToLower()))
+                {
+
+
+                    Log.Message(module.name);
+                    RimValiUtility.modulesFound = RimValiUtility.modulesFound + module.name + "\n";
+                    AvaliDefs.RimVali.race.baseHealthScale = RimValiMod.settings.healthScale;
+                    AvaliDefs.IWAvaliRace.race.baseHealthScale = RimValiMod.settings.healthScale;
+                    AvaliDefs.IWAvaliRace.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMax, value = RimValiMod.settings.IWAvaliMaxTemp });
+                    AvaliDefs.IWAvaliRace.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMin, value = RimValiMod.settings.IWAvaliMinTemp });
+                    AvaliDefs.RimVali.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMax, value = RimValiMod.settings.IlluminateAvaliMaxTemp });
+                    AvaliDefs.RimVali.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMin, value = RimValiMod.settings.IlluminateAvaliMinTemp });
+                    if (RimValiMod.settings.liteMode)
+                    {
+                        AvaliDefs.AvaliNanoForge.thingCategories = new List<ThingCategoryDef> { AvaliDefs.BuildingsSpecial };
+                        AvaliDefs.AvaliNanoForge.PostLoad();
+                        AvaliDefs.AvaliNanoForge.ResolveReferences();
+                        
+                        AvaliDefs.AvaliNanoLoom.thingCategories = new List<ThingCategoryDef> { AvaliDefs.BuildingsSpecial };
+                        AvaliDefs.AvaliNanoLoom.PostLoad();
+                        AvaliDefs.AvaliNanoLoom.ResolveReferences();
+
+                        AvaliDefs.AvaliResearchBench.thingCategories = new List<ThingCategoryDef> { AvaliDefs.BuildingsSpecial };
+                        AvaliDefs.AvaliResearchBench.PostLoad();
+                        AvaliDefs.AvaliResearchBench.ResolveReferences();
+                        AvaliDefs.AvaliResearchBench.designationCategory.PostLoad();
+                        AvaliDefs.AvaliResearchBench.designationCategory.ResolveReferences();
+                        foreach(ThingDef def in DefDatabase<ThingDef>.AllDefs.Where(x => x.IsWeapon && x.modContentPack.PackageId.ToLower() == "nezitheavali.rimvali"))
+                        {
+                            def.recipeMaker = new RecipeMakerProperties();
+                            def.weaponTags = new List<string>();
+                            def.tradeTags = new List<string>();
+
+                            def.PostLoad();
+                            def.ResolveReferences();
+                        }
+                        foreach (ThingDef def in DefDatabase<ThingDef>.AllDefs.Where(x => x.IsApparel && x.modContentPack.PackageId.ToLower() == "nezitheavali.rimvali"))
+                        {
+                            def.recipeMaker = new RecipeMakerProperties();
+                            def.weaponTags = new List<string>();
+                            def.tradeTags = new List<string>();
+                            
+                            def.PostLoad();
+                            def.ResolveReferences();
+                        }
+                    }
+                }
+            }
+           
+            //AvaliDefs.RimVali.race.baseHealthScale = RimValiMod.settings.healthScale;
+            //AvaliDefs.IWAvaliRace.race.baseHealthScale = RimValiMod.settings.healthScale;
+
+            /*
+            AvaliDefs.IWAvaliRace.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMax, value = RimValiMod.settings.IWAvaliMaxTemp });
+            AvaliDefs.IWAvaliRace.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMin, value = RimValiMod.settings.IWAvaliMinTemp });
+            AvaliDefs.RimVali.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMax, value = RimValiMod.settings.IlluminateAvaliMaxTemp });
+            AvaliDefs.RimVali.statBases.Add(new StatModifier() { stat = StatDefOf.ComfyTemperatureMin, value = RimValiMod.settings.IlluminateAvaliMinTemp });
+            */
+        }
+        static Modulefinder()
+        {
+            startup();
+        }
+    }
+}
